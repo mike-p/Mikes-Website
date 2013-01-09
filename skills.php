a1525461e17ad9143157404d8c859ab998e12a80
mike <mike-p.co.uk>
2013-01-09 16:36:05 +0000

    updates to skills

--- skills.php
+++ skills.php
@@ -12,15 +12,26 @@
 		<div class="main-content" role="main">
 			<h4>Management</h4>
 			<ul>
-				<li>Operations Management - Resource planning, allocation, budget and all areas of project delivery</li> 
-				<li>Business Analysis - Face to Face customer/client meetings, initial scoping, testing, sign off, change requests with a particular interest in e-commerce</li>
-				<li>Project Management - Planning deliverables, milestones, delivery</li>
+				<li><strong>Project Management</strong>
+					<ul>
+						<li>Scoping and creating business requirements; understanding the key deliverables and clearly communicating with stakeholders.</li>
+						<li>Responsible for creating project plans, ensuring a successful project delivery and managing change requests.</li>
+						
+						<li>Experience with data migration, re-platforming, integrating with legacy systems; both e-commerce and bespoke applications.</li>
+						
+						<li>Effectively managing project teams in accordance with their personalities and skill sets.</li>
+					</ul>
+				</li>
+				<li><strong>Operations Management</strong> - Resource planning, allocation, budget and all areas of project delivery</li> 
+				<li><strong>Business Analysis</strong> - Face to Face customer/client meetings, initial scoping, testing, wire-framing, sign off and managing change requests</li>
+				
+				<li>Quick to understand technical requirements in e-commerce environments; able to interface clearly between commercial and development teams.</li>	
 				<li>Mentoring junior developers and designers </li>
 				<li>Experience in both working and leading in <strong>Agile and Waterfall</strong> approaches</li>
 			</ul>
 			<h4>Qualifications</h4>
 			<ul>
-				<li>PRINCE2 Practitioner (2012) Candidate number: P2R/869563 - QA</li>
+				<li>PRINCE2 Practitioner (2012) Candidate number: P2R/869563 - QA (<a href="http://www.apmg-international.com/SCRQuery.aspx?exam=p2">APMG</a>)</li>
 				<li>BSc (Hons) Computing and Management – 2:1, Bradford University</li>
 			</ul>
 			<h4>Technical Skills</h4>
@@ -37,6 +48,7 @@
 			<li>Basic UML</li>
 			<li>Test Director/Mantis</li>
 			<li>Google Analytics &amp; Webmaster Tools</li>
+			<li>Basecamp; awareness of other tools such as Pivotal tracker</li>
 			</ul>
 			
 		</div>
