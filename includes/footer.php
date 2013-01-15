4f0c55ead8ea3a6c6e0d8332c1b1b5ed1d1480f0
mike <mike-p.co.uk>
2013-01-15 16:08:30 +0000

    update

--- includes/footer.php
+++ includes/footer.php
@@ -10,6 +10,6 @@
 			</address>
 			
 			 
-			<p>Care to validate the <a href="http://validator.w3.org/check/referer" title="Opens the W3 site up"><abbr title="HyperText Markup Language">HTML</abbr></a> or <a href="http://jigsaw.w3.org/css-validator/check/referer"><abbr title="Cascading Style Sheets">CSS</abbr></a> | Made with a Mac in London</p>
+			<p>Care to validate the <a href="http://validator.w3.org/check/referer" title="Opens the W3 site up" rel="nofollow"><abbr title="HyperText Markup Language">HTML</abbr></a> or <a href="http://jigsaw.w3.org/css-validator/check/referer" rel="nofollow"><abbr title="Cascading Style Sheets">CSS</abbr></a> | Made with a Mac in London</p>
 		</footer>
 		
