3ca031f1ac68bf1e5922e6b490b4f4ee7d187581
mike-p <mike-p@users.noreply.github.com>
2020-09-02 15:46:29 +0100

    Job title tweaks

--- index.php
+++ index.php
@@ -1,31 +1,33 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr" itemscope itemtype="http://schema.org/Person">
+
 <head>
 	<?php include 'includes/header-includes.php' ?>
 	<link rel="prev" title="contact" href="http://mike-p.co.uk/contact" />
 	<link rel="next" title="skills" href="http://mike-p.co.uk/skills" />
 
-	<title>Home | Mike Smith | Senior digital product manager for LEGO.com</title>
+	<title>Home | Mike Smith | Head of Product</title>
 
 </head>
+
 <body>
 	<div class="inner-body">
 		<header>
 			<nav>
 				<ul>
-				<li><a class="active" href="/">Home</a></li>
-				<li><a href="about" title="My core skills">About</a></li> 
-				<li><a href="interests" title="Some of my interests">Fun</a> </li>
-				<li><a href="contact" title="Lets talk">Lets Talk</a></li>
-				</ul> 
+					<li><a class="active" href="/">Home</a></li>
+					<li><a href="about" title="My core skills">About</a></li>
+					<li><a href="interests" title="Some of my interests">Fun</a> </li>
+					<li><a href="contact" title="Lets talk">Lets Talk</a></li>
+				</ul>
 			</nav>
 		</header>
 		<div class="main-content" role="main">
 			<h4>Hi!</h4>
-			
+
 			<p>My name is Mike Smith and I am an experienced digital <strong>head of product</strong> working at <a href="http://news.co.uk" rel="nofollow">News UK</a>.</p>
 
-			<p>I have been involved in the interweb for the last 18 years at some great <a href="about">places</a> and in my spare time have a couple of  <a href="/interests">interests</a> to keep me busy.</p>
+			<p>I have been involved in the interweb for the last 18 years at some great <a href="about">places</a> and in my spare time have a couple of <a href="/interests">interests</a> to keep me busy.</p>
 		</div>
 		<footer>
 			<nav id="nav" class="global-nav">
@@ -36,7 +38,7 @@
 					<li><a href="/interests" title="Some of my interests">Fun</a></li>
 					<li class="contact"><a href="/contact" title="Lets chat">Lets Talk</a></li>
 				</ul>
-				
+
 			</nav>
 			<?php include 'includes/about.php' ?>
 			<?php include 'includes/colophon.php' ?>
@@ -45,4 +47,5 @@
 	</div>
 	<?php include 'includes/analytics.php' ?>
 </body>
+
 </html>
--- work.php
+++ work.php
@@ -1,58 +1,60 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr" itemscope itemtype="http://schema.org/Person">
+
 <head>
 	<?php include 'includes/header-includes.php' ?>
 	<link rel="prev" title="skills" href="http://mike-p.co.uk/skills" />
 	<link rel="next" title="interests" href="http://mike-p.co.uk/interests" />
-	<title>Work | Mike Smith | Senior digital product manager for LEGO.com</title>
+	<title>Work | Mike Smith | Head of Product</title>
 </head>
+
 <body id="work">
 	<div class="inner-body">
 		<header>
-				<nav>
+			<nav>
 				<ul>
-				<li><a href="/">Home</a></li>
-				<li><a href="about" title="My core skills">About</a></li> 
-				<li><a class="active" href="work" title="A bit about my work history">Work</a></li>
-				<li><a href="interests" title="Some of my interests">Fun</a> </li>
-				<li><a href="contact" title="Lets talk">Lets Talk</a></li>
-				</ul> 
+					<li><a href="/">Home</a></li>
+					<li><a href="about" title="My core skills">About</a></li>
+					<li><a class="active" href="work" title="A bit about my work history">Work</a></li>
+					<li><a href="interests" title="Some of my interests">Fun</a> </li>
+					<li><a href="contact" title="Lets talk">Lets Talk</a></li>
+				</ul>
 			</nav>
 		</header>
 		<div class="main-content" role="main">
-			<p>I have had the pleasure of working at some amazing places with some very talented people...</p> 
+			<p>I have had the pleasure of working at some amazing places with some very talented people...</p>
 			<h4>LEGO.com</h4>
 			<h5>Senior product manager</h5>
-			<p>As the product manager for LEGO.com I'm involved in a whole bunch of things that I can't really reveal here.  But they are awesome.</p>
-			
+			<p>As the product manager for LEGO.com I'm involved in a whole bunch of things that I can't really reveal here. But they are awesome.</p>
+
 			<h4>Which?</h4>
 			<h5>Product manager</h5>
-			<p>Working as a digital product manager on various parts of the Which? re-estate.  Notable achievements was the re-launch of reviews and consumer rights responsive websites.</p>
+			<p>Working as a digital product manager on various parts of the Which? re-estate. Notable achievements was the re-launch of reviews and consumer rights responsive websites.</p>
 
-			
+
 			<h4>Zoocha</h4>
 			<h5>Strategy director</h5>
-			<p>I co-founded Zoocha in 2009 with the aim for it to become the leading Drupal development agency in the UK.  Within 3 years we built up a client base that includes Argos, Boden, Debenhams, Gamesworkshop, Mydex, Oceans Rattan Furniture and Tesco to name but a few and completed a diverse range of projects (e-commerce, property portals to Facebook apps).</p>
-						
-						
+			<p>I co-founded Zoocha in 2009 with the aim for it to become the leading Drupal development agency in the UK. Within 3 years we built up a client base that includes Argos, Boden, Debenhams, Gamesworkshop, Mydex, Oceans Rattan Furniture and Tesco to name but a few and completed a diverse range of projects (e-commerce, property portals to Facebook apps).</p>
+
+
 			<h4>Sorriso</h4>
 			<h5>Consultant</h5>
-			<p>I worked at Sorriso for a year in between my trips to South America and West Africa.  I was brought in to utilise my experience with Telecoms and building web applications and was responsible for building out the front-end of a universal online billing application.</p>
-			
+			<p>I worked at Sorriso for a year in between my trips to South America and West Africa. I was brought in to utilise my experience with Telecoms and building web applications and was responsible for building out the front-end of a universal online billing application.</p>
+
 			<h4>RI3K (Qatarlyst)</h4>
 			<h5>Developer</h5>
-			<p>RI3K provide an online trading service for the global insurance and reinsurance industry.  I was a lead Interface Developer and introduced front end standards across the various platforms and helped mentor junior developers.</p>
-						
+			<p>RI3K provide an online trading service for the global insurance and reinsurance industry. I was a lead Interface Developer and introduced front end standards across the various platforms and helped mentor junior developers.</p>
+
 			<h4>Oracle</h4>
 			<h5>Consultant</h5>
 			<p>Joining Oracle as a Consultant I progressed to becoming a Technical Business Analyst on various telecoms projects based through Europe using the various Oracle platforms.</p>
-			
+
 			<h4>Tesco.com</h4>
 			<h5>Developer</h5>
 			<p>Where it all began, arriving as a fresh faced graduate I joined the content team and progressed to the development team working on all elements of the Tesco.com team and learning the joys of e-commerce.</p>
-			
-					
-			
+
+
+
 		</div>
 	</div>
 	<footer>
@@ -65,12 +67,13 @@
 				<li><a href="/interests" title="Some of my interests">Interests</a></li>
 				<li class="contact"><a href="/contact" title="Lets chat">Lets Talk</a></li>
 			</ul>
-			
+
 		</nav>
 		<?php include 'includes/about.php' ?>
 		<?php include 'includes/colophon.php' ?>
 	</footer>
-	
+
 	<?php include 'includes/analytics.php' ?>
 </body>
+
 </html>
