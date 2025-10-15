608eb534f3ca6e5f39187ac6159ee3a0f0cc3161
mike-p <mike-p@users.noreply.github.com>
2025-10-15 10:51:14 +0100

    Update about.php

--- includes/about.php
+++ includes/about.php
@@ -1,5 +1,5 @@
 <div class="contact-me">
 				<h4>Let's Connect!</h4>
 				<p>I'm always interested in exploring new opportunities in product leadership, AI strategy, and transformative product development.</p>
-				<p><a class="primary-cta" href="mailto:mike.p.smith@gmail.com">Get in touch by email</a> | <a class="secondary-cta" href="https://www.linkedin.com/in/mikepfsmith/" target="_blank">Connect via LinkedIn</a></p>
+				<p><a class="primary-cta" href="mailto:mike.p.smith@gmail.com?subject=Hello%20from%20your%20website">Get in touch by email</a> | <a class="secondary-cta" href="https://www.linkedin.com/in/mikepfsmith/" target="_blank">Connect via LinkedIn</a></p>
 			</div>
