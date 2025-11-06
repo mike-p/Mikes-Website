dd6067d00205d0d718f5abd477cf2d38df5aa9cd
mike-p <mike-p@users.noreply.github.com>
2025-11-06 09:55:08 +0000

    Sort out URL conventions and SEO stuff

--- includes/colophon.php
+++ includes/colophon.php
@@ -3,7 +3,7 @@
 		
 		<div class="colophon">	
 			<address class="vcard">
-			<p>Copyright &copy; <?php echo date('Y'); ?> <a href="https://mike-p.co.uk/" class="url" rel="me">Mike Smith</a>.</p> 
+		<p>Copyright &copy; <?php echo date('Y'); ?> <a href="https://mike-p.co.uk" class="url" rel="me">Mike Smith</a>.</p> 
 			<p><a href="mailto:hello@mike-p.co.uk" class="email fn">Mike Smith</a> is a <span class="role">Product Leader</span> living very near <span class="adr"><span class="locality">London</span>, <span class="country-name">England</span></span>.
 			| <a href="https://mike-p.co.uk/work" title="Hire me!">Work</a></p>
 			</address>
